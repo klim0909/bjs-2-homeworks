8501fc887f06e7cc9cf841bdd295e72852bc17fe
klim0909 <121249743+klim0909@users.noreply.github.com>
2023-08-14 15:22:55 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+  } 
+  this.marks.push(mark);
 }
+Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+  } 
+  this.marks.push(...mark);
+}
+
 
 Student.prototype.getAverage = function () {
   
+  let average = this.marks.reduce((acc,item,idx,arr)=> {
+    if (idx === arr.length - 1){
+      return (acc+item)/ arr.length;
+    }
+    else {
+      return acc+item;
+    }
+  }, 0)
+  return average;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+
+  delete this.marks;
+  delete this.subject;
 }
